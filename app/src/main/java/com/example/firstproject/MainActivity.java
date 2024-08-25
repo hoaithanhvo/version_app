34739a0d0fcb51fe12a5a810faf2116ce3c7f3b6
hoaithanhvo <115620157+hoaithanhvo@users.noreply.github.com>
2024-08-25 23:02:14 +0700

    update code

--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
 import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -20,9 +29,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvFileName;
     private Button btnDownload, btnView;
 
-    private String filepath = "http://africau.edu/images/default/sample.pdf";
+    private String filepath = "https://github.com/dangquanuet/wifi-hunter-public/raw/develop/apk/wifi-hunter-version-1-0-2.apk";
     private URL url = null;
     private String fileName;
+    private String filePath;
+    private long downloadID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +42,15 @@ public class MainActivity extends AppCompatActivity {
 
         initViews();
         setListeners();
+
+        // Đăng ký BroadcastReceiver để lắng nghe sự kiện tải xong
+        registerReceiver(onDownloadComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
     }
 
     private void initViews() {
         tvFileName = findViewById(R.id.tvUrl);
         btnDownload = findViewById(R.id.btnDownload);
         btnView = findViewById(R.id.btnView);
-
 
         try {
             url = new URL(filepath);
@@ -47,32 +60,94 @@ public class MainActivity extends AppCompatActivity {
 
         fileName = url.getPath();
         fileName = fileName.substring(fileName.lastIndexOf('/') + 1);
+        filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/";
         tvFileName.setText(fileName);
     }
 
     private void setListeners() {
+        btnDownload.setOnClickListener(v -> downloadFileFromUrl(filepath));
 
-        btnDownload.setOnClickListener(v -> {
-            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url + ""));
-            request.setTitle(fileName);
-            request.setMimeType("applcation/pdf");
-            request.allowScanningByMediaScanner();
-            request.setAllowedOverMetered(true);
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
-            DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-            dm.enqueue(request);
+        btnView.setOnClickListener(v -> {
+            // Lấy URI của file từ DownloadManager
+            Uri fileUri = getUriFromDownloadManager(this, downloadID);
+            if (fileUri != null) {
+                openApkFile(fileUri);
+            } else {
+                Toast.makeText(this, "File does not exist", Toast.LENGTH_SHORT).show();
+            }
         });
-
-    btnView.setOnClickListener(v -> {
-        File file=new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"/"+fileName);
-        Uri uri= FileProvider.getUriForFile(MainActivity.this,"com.example.firstproject"+".provider",file);
-        Intent i=new Intent(Intent.ACTION_VIEW);
-        i.setDataAndType(uri,"application/pdf");
-        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        startActivity(i);
-
-    });
     }
 
+    private void downloadFileFromUrl(String url) {
+        DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+
+        // Đặt vị trí tải về là thư mục Download
+        File dir = new File(filePath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        Uri downloadLocation = Uri.fromFile(new File(dir, fileName));
+
+        // Bắt đầu tải file
+        DownloadManager.Request request = new DownloadManager.Request(uri)
+                .setTitle(fileName)
+                .setDescription("Downloading...")
+                .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI)
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setDestinationUri(downloadLocation);
+        request.allowScanningByMediaScanner();
+        downloadID = downloadManager.enqueue(request);
+    }
+
+    private void openApkFile(Uri uri) {
+        if (uri != null) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/vnd.android.package-archive");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "File does not exist", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private Uri getUriFromDownloadManager(Context context, long downloadId) {
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Cursor cursor = downloadManager.query(new DownloadManager.Query().setFilterById(downloadId));
+        if (cursor != null && cursor.moveToFirst()) {
+            int columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI);
+            if (columnIndex != -1) {
+                String fileUriString = cursor.getString(columnIndex);
+                cursor.close();
+                return Uri.parse(fileUriString);
+            }
+            cursor.close();
+        }
+        return null;
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        // Hủy đăng ký BroadcastReceiver khi Activity bị hủy
+        unregisterReceiver(onDownloadComplete);
+        super.onDestroy();
+    }
+
+    private final BroadcastReceiver onDownloadComplete = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+            if (downloadID == id) {
+                // Lấy URI của file từ DownloadManager
+                Uri fileUri = getUriFromDownloadManager(context, id);
+                if (fileUri != null) {
+                    Toast.makeText(context, "Download Complete", Toast.LENGTH_SHORT).show();
+                    //openApkFile(fileUri);
+                } else {
+                    Toast.makeText(context, "File does not exist", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
 }
